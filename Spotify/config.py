97c4528e456c375adf20774ef991110e01d26568
vasaviseshu02 <78820746+vasaviseshu02@users.noreply.github.com>
2021-03-06 18:46:27 -0600

    removed credentials

--- Spotify/config.py
+++ Spotify/config.py
@@ -1,9 +1,9 @@
-client_id = '3ff93659a2264e4ebb65ff3cd83959b6'
-client_secret = 'b28979a467e44a3697cb6787467a6607'
+client_id = ''
+client_secret = ''
 
-myHostname = "mccj0rytz641cljpn88j300hmdt1.ftp.marketingcloudops.com"
-myUsername = "520000018_2"
-myPassword = "472142df89c497cc6b7e94d54bd749bd!"
+myHostname = ""
+myUsername = ""
+myPassword = "!"
 
 OutputFileName = 'Spotify_Full_Related_Artists_'
 FileName = "Master_Artist_List_2020_12_18.csv"
